02576268f12d15fa4e02dc763477c2cfffa6bfb7
Vnscarli <vnsmendes7@gmail.com>
2026-01-29 07:07:09 -0300

    Adding tests for ripple_carry_adder

--- FullAdder/full_adder.sage
+++ FullAdder/full_adder.sage
@@ -1,52 +1,44 @@
 load('DGHV.sage')
 
-dghv = DGHV(2000, 160, 15)
 
 
-def full_adder(dghv, ca, cb, carryenc):
-    #ca = dghv.enc(bit_a)
-    #cb = dghv.enc(bit_b)
-    #carryenc = dghv.enc(carry)
+
+def full_adder(dghv_inst, ca, cb, carryenc):
+    #ca = dghv_inst.enc(bit_a)
+    #cb = dghv_inst.enc(bit_b)
+    #carryenc = dghv_inst.enc(carry)
 
     # Sum the numberes
-    axorb = dghv.add(ca, cb)
-    soma = dghv.add(axorb, carryenc)
+    axorb = dghv_inst.add(ca, cb)
+    soma = dghv_inst.add(axorb, carryenc)
 
     # Carry out of the sum
     # Carryout = (A AND B) XOR (Carryin AND (A XOR B))
-    a_and_b = dghv.mult(ca, cb)
-    carry_and_axorb = dghv.mult(carryenc, axorb)
-    carry_out = dghv.add(a_and_b, carry_and_axorb)
+    a_and_b = dghv_inst.mult(ca, cb)
+    carry_and_axorb = dghv_inst.mult(carryenc, axorb)
+    carry_out = dghv_inst.add(a_and_b, carry_and_axorb)
     return soma, carry_out
 
-def ripple_carry_adder(list_a, list_b):
+def ripple_carry_adder(list_a, list_b, dghv_inst):
     if len(list_a) != len(list_b):
         raise ValueError("Numbers must have the same number of digits")
     
     # Starts carry with 0
-    c_carry = dghv.enc(0)
+    c_carry = dghv_inst.enc(0)
 
     # Create a list for the answer
     ans =[]
 
     for a,b in zip(reversed(list_a), reversed(list_b)):
         # Sum the numbers from the LSB to MSB
-        ca = dghv.enc(a)
-        cb = dghv.enc(b)
+        ca = dghv_inst.enc(a)
+        cb = dghv_inst.enc(b)
         
-        s, c_carry = full_adder(dghv, ca, cb, c_carry)
-        c_carry = dghv.bootstrap(c_carry)
-        bootstrap_s = dghv.bootstrap(s)
-        ans.append(bootstrap_s)
+        s, c_carry = full_adder(dghv_inst, ca, cb, c_carry)
+        c_carry = dghv_inst.bootstrap(c_carry)
+        s = dghv_inst.bootstrap(s)
+        ans.append(s)
 
     # The result was created in reversed so need to reverse it back
     return ans[::-1]
 
-byte_a = [0,0,0,0,1,0,1,1]
-byte_b = [0,0,0,0,0,1,1,1]
-
-sum_result = ripple_carry_adder(byte_a, byte_b)
-
-# The sum is wrong, what happened here?
-decoded_result = [dghv.dec(c) for c in sum_result]
-print(f"Decrypted bits: {decoded_result}")
--- FullAdder/test.py
+++ FullAdder/test.py
@@ -1,44 +1,43 @@
 import pytest
 from sage.all import *
-import math
 
 
 load('DGHV.sage')
-load('full_adder.sage')
+load('full_adder.sage') 
 
 @pytest.fixture(scope="module")
 def dghv_inst():
-    rho = 15 
-    eta = 160
-    gamma = 2000
-    return DGHV(gamma, eta, rho)
+    # DGHV parameters
+    return DGHV(3000, 250, 20)
 
-def get_noise_bits(dghv, ciphertext):
-    noise = (ciphertext % dghv.p) 
-    return float(log(noise, 2)) if noise > 0 else 0
+def int_to_bits(n, width=8):
+    # Convert integet o bits
+    return [int(b) for b in bin(n)[2:].zfill(width)]
 
-@pytest.mark.parametrize("a, b, cin, expected_s, expected_cout", [
-    (0, 0, 0, 0, 0),
-    (0, 0, 1, 1, 0),
-    (0, 1, 0, 1, 0),
-    (0, 1, 1, 0, 1),
-    (1, 0, 0, 1, 0),
-    (1, 0, 1, 0, 1),
-    (1, 1, 0, 0, 1),
-    (1, 1, 1, 1, 1),
+def bits_to_int(bits):
+    # Converts bits back to integers
+    return int("".join(map(str, bits)), 2)
+
+@pytest.mark.parametrize("val_a, val_b", [
+    (11, 7),    
+    (255, 1),   # Tests overflow
+    (0, 0),    
+    (128, 127), # 255
+    (32, 56),
+    (88, 243)
 ])
-def test_full_adder_logic(dghv_inst, a, b, cin, expected_s, expected_cout):
-   
-    ca = dghv_inst.enc(a)
-    cb = dghv_inst.enc(b)
-    ccin = dghv_inst.enc(cin)
+def test_8bit_ripple_carry_adder(dghv_inst, val_a, val_b):
+    # function to test the ripple carry adder
+    bits_a = int_to_bits(val_a)
+    bits_b = int_to_bits(val_b)
+    expected_sum = (val_a + val_b) % 256  # makes sure the output is 8 bits long
     
-    s_enc, cout_enc = full_adder(dghv_inst, ca, cb, ccin)
+    # Run the adder
+    encrypted_res_bits = ripple_carry_adder(bits_a, bits_b, dghv_inst)
     
-    s_noise = get_noise_bits(dghv_inst, s_enc)
-    c_noise = get_noise_bits(dghv_inst, cout_enc)
-   
-    print(f"\nCase ({a},{b},{cin}) -> Sum Noise: {s_noise:.2f} bits, Carry Noise: {c_noise:.2f} bits")
+    # Verification if code is working fine
+    decrypted_bits = [dghv_inst.dec(c) for c in encrypted_res_bits]
+    actual_sum = bits_to_int(decrypted_bits)
     
-    assert dghv_inst.dec(s_enc) == expected_s, f"Sum failed for ({a},{b},{cin})"
-    assert dghv_inst.dec(cout_enc) == expected_cout, f"Carry failed for ({a},{b},{cin})"
+    # Asserts the value of the decrypted sum and the actual sum of the numbers mod 256
+    assert actual_sum == expected_sum, f"Sum failed! Sum expected: {expected_sum}.Sum obtained: {actual_sum}"
